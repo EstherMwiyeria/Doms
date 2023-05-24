9af238bba027d49c700bef23cfed2b6e2e74f970
EstherMwiyeria <esthermkanoro@gmail.com>
2023-05-24 16:27:01 +0300

    DOM commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// Change document background color to
+// silver
+document.body.style.backgroundColor="silver";
+
+// Change the font color for h1 title tag to
+// green
+document.getElementById("title").style.color="green";
+
+// Change the font case for h3 title tags to
+// uppercase
+let h3Tags = document.getElementsByTagName("h3");
+for (let i = 0; i < h3Tags.length; i++) {
+  h3Tags[i].style.textTransform = "uppercase";
+}
+
+// Add one more fruit to the fruits list
+let li = document.createElement("li")
+li.innerHTML = "Grapes";
+document.getElementById("fruList").appendChild(li)
+
+// Add one more vegetable to the vegetables
+// list
+// let li = document.createElement("li")
+// li.innerHTML = "Capsicles";
+// document.getElementById("vegList").appendChild(li)
